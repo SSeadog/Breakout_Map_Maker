4f8be7f962d8c64b83193c7a37e0a1c82d1c20fb
Seadog1225 <aasssa1225@naver.com>
2020-11-05 22:42:22 +0900

    Add MouseEvent For Attaching Brick

--- com/seadog/MapMaker.java
+++ com/seadog/MapMaker.java
@@ -11,5 +11,46 @@ public class MapMaker extends JFrame {
         setSize(300, 400);
         setLocation(300, 300);
         setVisible(true);
+        setContentPane(new MyPanel());
     }
+
+    class MyPanel extends JPanel {
+        public MyPanel() {
+            setLayout(null);
+            addMouseListener(new MyMouseAdapter());
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+        }
+    }
+
+    public static void main(String[] args) {
+        new MapMaker();
+    }
+
+    class MyMouseAdapter extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            // 추후에 겹쳐서 만들어지는 경우를 생각해야 할듯
+            int x = e.getX();
+            int y = e.getY();
+
+            add(new Brick(x, y));
+            repaint();
+        }
+    }
+
+    class Brick extends JLabel {
+        public Brick(int x, int y) {
+            setText("Brick");
+            setSize(40, 10);
+            setLocation(x, y);
+        }
+    }
+
 }
+
+// Breakout에서 Brick들을 생성할 때 필요한 정보는 총 브릭의 개수, 각 브릭의 위치가 있으면 될듯
+// 배열을 생성해서 각 항마다 좌표를 주면 해결될 듯
+
+// 맵 생성시에는?
