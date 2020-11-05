5502946bb14ce0f3968ea60fd76f2dec3da67637
Seadog1225 <aasssa1225@naver.com>
2020-11-05 22:19:19 +0900

    Initializing Project

---
+++ com/seadog/MapMaker.java
@@ -0,0 +1,15 @@
+package com.seadog;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MapMaker extends JFrame {
+    public MapMaker() {
+        setTitle("맵 만들기");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLocation(300, 300);
+        setVisible(true);
+    }
+}
